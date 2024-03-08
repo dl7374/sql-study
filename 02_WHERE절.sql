48671e18361f4da3eacad5e56672b43160bf833a
논스톱 <dl7374@naver.com>
2024-03-08 17:53:11 +0900

    sql 첫 공부

---
+++ 02_WHERE절.sql
@@ -0,0 +1,20 @@
+
+select * from employees;
+
+-- WHERE절 비교 (데이터 값은 대/소문자를 구분합니다.)
+SELECT first_name, last_name, job_id
+from employees
+WHERE job_id = 'IT_PROG';
+
+SELECT *
+FROM employees
+WHERE last_name = 'King';
+
+
+
+
+
+
+
+
+
